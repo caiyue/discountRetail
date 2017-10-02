2bb2d1921fbdccf1c27ea8797210341625ca5d0e
北京-蔡月 <caiyue@jd.com>
2017-10-02 17:10:17 +0800

    init

--- discountRetail/urls.py
+++ discountRetail/urls.py
@@ -1,3 +1,5 @@
+#encoding:utf-8
+
 """discountRetail URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,7 +17,22 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from discountRetailApp import views as discountRetail_views
 
 urlpatterns = [
+    # in the past,django use include method
     url(r'^admin/', admin.site.urls),
+    url(r'^$',discountRetail_views.index),
+    #u^add add 开头
+    #add$ add结尾
+    # ^add$ add开头结尾的，也就是完全匹配add
+    # add 只要包括add 就可以匹配
+    url(r'^add$',discountRetail_views.add,name='add'),
+    url(r'^cal',discountRetail_views.cal,name='cal2'),
+
+    url(r'^minus',discountRetail_views.minus,name='minus'),
+    # url(r'^add2/(\d+)/(\d+)/$',discountRetail_views.add2,name='add2'),
+    url(r'^add/(\d+)/(\d+)/$', discountRetail_views.add2, name='addfunc'),
+    url(r'^people', discountRetail_views.people, name='people'),
+    url(r'^base', discountRetail_views.base, name='base'),
 ]
--- discountRetailApp/views.py
+++ discountRetailApp/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+#encoding:utf-8
 
+from django.shortcuts import render
+from django.http  import HttpResponse
 # Create your views here.
+
+def base(request):
+    return render(request, 'discountRetailApp/base.html', {'tstring': '测试字符串'})
+
+def index(request):
+    # return HttpResponse('this is a test page')
+    contentList = ['item1','item2','item3','item4','item5']
+    return  render(request, 'discountRetailApp/index.html',{'tstring':'测试字符串'})
+    # print  contentList
+    # return  render(request, 'discountRetailApp/index.html',{'clist':contentList})
+
+def cal(request):
+    return render(request, 'discountRetailApp/add.html')
+
+def people(request):
+    return render(request, 'discountRetailApp/people.html')
+
+def add(request):
+    a = int(request.GET['a'])
+    b = int(request.GET['b'])
+    return HttpResponse('add sum = %d' % (a+b))
+
+
+def add2(request,a,b):
+    #add2/10/20/?c=100&d=30
+    dict = request.GET
+    c = d = 0
+    if dict.has_key('c'):
+        c = dict['c']
+    if dict.has_key('d'):
+        d = dict['d']
+    return HttpResponse('add2 sum == %d' % (int(a) + int(b) + int(c) + int(d)))
+
+def minus(request):
+    a = int(request.GET['a'])
+    b = int(request.GET['b'])
+    return HttpResponse('result = %d'%(a-b))
+
