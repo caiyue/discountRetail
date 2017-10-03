145fb0be57f688c594166a1e9cfde332a7bd7b9e
北京-蔡月 <caiyue@jd.com>
2017-10-03 16:10:01 +0800

    no message

--- discountRetailApp/admin.py
+++ discountRetailApp/admin.py
@@ -1,3 +1,48 @@
+#encoding:utf-8
 from django.contrib import admin
-
 # Register your models here.
+from .models import Blog
+from  .models import  Person
+from  datetime import  datetime
+
+#可以显示的字段,在列表页显示，详情页的一些字段不会显示
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name','age','address')
+admin.site.register(Person,PersonAdmin)
+
+
+class BlogAdmin(admin.ModelAdmin):
+     # 可以显示的字段
+    list_display = ('title','content','auth','email')
+    search_fields =('auth',)
+    def get_search_results(self, request, queryset, search_term):
+        queryset,use_distinct = super(BlogAdmin,self).get_search_results(request,queryset,search_term)
+        try:
+            # name__contains or name__like 都可以
+            queryset |= self.model.objects.filter(name__contains=search_term)
+        except:
+            pass
+
+        return queryset,use_distinct
+
+
+    # obj 是blog 对象，这里可以直接更新blog 信息
+    def save_model(self, request, obj, form, change):
+        obj.auth = str(request.user)
+        obj.update_time = str(datetime.today())
+        obj.save()
+
+    # 重写delete 方法
+    def delete_model(self, request, obj):
+        obj.delete()
+
+
+    def get_queryset(self, request):
+        qs = super(BlogAdmin,self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(auth=request.user)
+
+
+admin.site.register(Blog,BlogAdmin)
--- discountRetailApp/models.py
+++ discountRetailApp/models.py
@@ -1,29 +1,33 @@
+#encoding:utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
-
 class   Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
+    address = models.CharField(max_length=100,default='暂无信息')
 
     def __unicode__(self):
         return self.name
 
 class   Blog(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
+    auth = models.CharField(u'作者',max_length=30,default='no author')
+    email = models.EmailField(u'邮箱')
+    title = models.CharField(u'标题',max_length=256,default='empty title')
+    update_time = models.CharField(u'更新时间',max_length=256,default=u'暂无更新')
+    content = models.TextField(u'内容',max_length=1000,default='empty content')
+
     def __unicode__(self):
-        return self.name
+        return self.title
 
 class   Entry(models.Model):
     blog = models.ForeignKey(Blog)
-    headline = models.CharField()
+    headline = models.CharField(max_length=30)
     body_text = models.TextField()
     pub_date = models.DateField()
     mod_date = models.DateField()
-    authors = models.ManyToManyField()
     n_comments = models.IntegerField()
     n_pingbacks = models.IntegerField()
     rating = models.IntegerField()
@@ -31,3 +35,13 @@ class   Entry(models.Model):
     def __unicode__(self):
         return self.headline
 
+
+class   Student(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=11)
+
+    def __unicode__(self):
+        return self.name
